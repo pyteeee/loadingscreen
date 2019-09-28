58f320bb72b97090969d650094f173514cbc622b
sergiorei <54647024+sergiorei@users.noreply.github.com>
2019-09-28 22:30:51 +0100

    Add files via upload

---
+++ FiveM-Loadingscreen-Neon-master/Assets/JS/config.js
@@ -0,0 +1,21 @@
+var video_ID = "UdB4ytHvX8o" // ID de la video Youtube
+var mp3_player = true // Si true, le lecteur MP3 sera utilisé et le son de vidéo youtube coupé
+
+var start = 22 // Permets de définir à partir de combien de secondes la vidéo Youtube doit commencer
+
+var img = "Logo.png" // Le nom de fichier du logo de votre serveur (* Il doit se situer dans le dossier : Assets/IMG *)
+
+var Discord_link = "https://discord.gg/Kj3Z2SP" // Lien de vôtre server Discord
+
+var red_text = 'LION RP' // Texte en rouge
+var blue_text = 'ROLEPLAY' // Texte en bleu
+
+// N'oubliez d'ajouter vos musiques dans le __resource.lua
+// Placez vos musiques dans le dossier : Assets/AUIDO
+var tracks
+tracks = [{
+	"track": 1, // Position dans la liste
+	"name": "The huzz - Sensacional REMIX ft. Strychniness (Prod. the huzz)", // Le nom qui va apparête
+	"duration": "",
+	"file": "The huzz - Sensacional REMIX ft. Strychniness (Prod. the huzz)" // Le nom du fichier MP3 (Il ne doit pas contenir d'espace ou de caractère spéciaux.
+}]
